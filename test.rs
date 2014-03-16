a7ffa4cb98b2be50f37597c0ad02a6102a7ab0ee
Anders Aarsæther <anders@capasit.net>
2014-03-16 23:21:22 +0100

    Added some god damn normals

--- test.rs
+++ test.rs
@@ -25,6 +25,46 @@ static MAP_SIZE: uint = MAP_W * MAP_H;
 static VS_SRC: &'static str = "test.vert";
 static FS_SRC: &'static str = "test.frag";
 
+
+// Since cgmath doesn't compile yet...
+struct Vec3 {
+  x: GLfloat,
+  y: GLfloat,
+  z: GLfloat
+}
+
+impl Vec3 {
+
+  pub fn new(x: GLfloat, y: GLfloat, z: GLfloat) -> Vec3 {
+    Vec3 { x: x, y: y, z: z }
+  }
+
+  pub fn cross(&self, other: &Vec3) -> Vec3 {
+    Vec3::new((self.y * other.z) - (self.z * other.y),
+              (self.z * other.x) - (self.x * other.z),
+              (self.x * other.y) - (self.y * other.x))
+  }
+
+  pub fn add(&self, other: &Vec3) -> Vec3 {
+    Vec3::new(self.x + other.x, self.y + other.y, self.z + other.z)
+  }
+
+  pub fn sub(&self, other: &Vec3) -> Vec3 {
+    Vec3::new(self.x - other.x, self.y - other.y, self.z - other.z)
+  }
+
+  pub fn dot(&self) -> GLfloat {
+    self.x * self.x + self.y * self.y + self.z * self.z
+  }
+
+  pub fn normalize(&self) -> Vec3 {
+    let len = std::f32::sqrt(self.dot());
+    return Vec3::new(self.x / len, self.y / len, self.z / len);
+  }
+
+}
+
+
 #[start]
 fn start(argc: int, argv: **u8) -> int {
   native::start(argc, argv, main)
@@ -38,34 +78,60 @@ fn load_heightmap() -> ~[u8] {
   }
 }
 
-fn initialize_vertices(heightmap: ~[u8]) -> ~[GLfloat] {
-  let mut vertices: ~[GLfloat] = ~[(0.0 as GLfloat), ..(MAP_SIZE * 3)];
+fn initialize_vertices(heightmap: ~[u8]) -> ~[Vec3] {
+  let mut vertices: ~[Vec3] = ~[];
   let mut i = 0;
 
   for x in range(0, MAP_W) {
     for y in range(0, MAP_H) {
-      vertices.push(x as GLfloat);
-      vertices.push(y as GLfloat);
-      vertices.push(heightmap[(y * MAP_W) + x] as GLfloat);
+      let v = Vec3::new(x as GLfloat, y as GLfloat, heightmap[(y * MAP_W) + x] as GLfloat);
+      vertices.push(v);
+      // vertices.push(x as GLfloat);
+      // vertices.push(y as GLfloat);
+      // vertices.push(heightmap[(y * MAP_W) + x] as GLfloat);
     }
   }
   vertices
 }
 
-// fn initialize_normals(vertices: ~[GLfloat]) -> ~[GLfloat] {
-//   for x in range(0, MAP_W) {
-//     for y in range(0, MAP_H) {
-//
-//       let vx = MAP_W * x;
-//       let vy = y;
-//
-//       let mut cur: ~[GLfloat, ..3];
-//
-//       if x+1 < MAP_W && y+1 < MAP_H {
-//       }
-//     }
-//   }
-// }
+fn initialize_normals(v: ~[Vec3]) -> ~[Vec3] {
+  let mut normals: ~[Vec3] = ~[];
+
+  for row in range(0, MAP_W) {
+    for col in range(0, MAP_H) {
+
+      let hr = MAP_W * row;
+      let hc = col;
+
+      let sum = Vec3::new(0f32, 0f32, 0f32);
+      let cur = v[hr+hc];
+
+      if row+1 < MAP_W && col+1 < MAP_H {
+        let vec = (v[hr+0 + hc+1].sub(&cur)).cross(&v[hr+1 + hc+0].sub(&cur)).normalize();
+        sum.add(&vec);
+      }
+
+      if row+1 < MAP_W && col > 0 {
+        let vec = (v[hr+1 + hc+0].sub(&cur)).cross(&v[hr+0 + hc+1].sub(&cur)).normalize();
+        sum.add(&vec);
+      }
+
+      if row > 0 && col > 0 {
+        let vec = (v[hr+0 + hc+1].sub(&cur)).cross(&v[hr+1 + hc+0].sub(&cur)).normalize();
+        sum.add(&vec);
+      }
+
+      if row > 0 && col+1 < MAP_H {
+        let vec = (v[hr+1 + hc+0].sub(&cur)).cross(&v[hr+0 + hc+1].sub(&cur)).normalize();
+        sum.add(&vec);
+      }
+
+      normals.push(sum.normalize());
+
+    }
+  }
+  normals
+}
 //
 //     VecMat normals( hm );
 //     for( int col = 0; col < hm.cols(); ++col )
@@ -130,11 +196,11 @@ fn link_program(vs: GLuint, fs: GLuint) -> GLuint {
 
     // Fail on error
     if status != (gl::TRUE as GLint) {
-        let mut len: GLint = 0;
-        gl::GetProgramiv(program, gl::INFO_LOG_LENGTH, &mut len);
-        let mut buf = vec::from_elem(len as uint - 1, 0u8);     // subtract 1 to skip the trailing null character
-        gl::GetProgramInfoLog(program, len, ptr::mut_null(), buf.as_mut_ptr() as *mut GLchar);
-        fail!(str::raw::from_utf8(buf).to_owned());
+      let mut len: GLint = 0;
+      gl::GetProgramiv(program, gl::INFO_LOG_LENGTH, &mut len);
+      let mut buf = vec::from_elem(len as uint - 1, 0u8);     // subtract 1 to skip the trailing null character
+      gl::GetProgramInfoLog(program, len, ptr::mut_null(), buf.as_mut_ptr() as *mut GLchar);
+      fail!(str::raw::from_utf8(buf).to_owned());
     }
   }
   program
